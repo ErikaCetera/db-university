694fb7e1ae3eebc94172f87c4d48bdcd32bf537c
ErikaCetera <ceteraerika@gmail.com>
2025-01-20 18:43:23 +0100

    feat: queries bonus

--- queries.sql
+++ queries.sql
@@ -9,7 +9,10 @@ FROM `courses`
 WHERE `cfu`> 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni 
- 
+ SELECT *
+FROM `students`
+WHERE YEAR(`date_of_birth`) <= 1994
+ORDER BY  `date_of_birth`;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT COUNT(*)
@@ -36,3 +39,29 @@ FROM `departments`;
 SELECT COUNT(*)
 FROM `teachers`
 WHERE `phone` IS NULL;
+
+
+
+--BONUS
+
+--1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(*), YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+--2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(*), `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
+--3. Calcolare la media dei voti di ogni appello d'esame
+SELECT AVG(`vote`) , `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`
+ORDER BY `exam_id`;
+
+--4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT  COUNT(*), `department_id`
+FROM `degrees`
+GROUP BY `department_id`;
+
