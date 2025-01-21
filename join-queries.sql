bf67f6afcd97948e3143f4100135848fca305403
ErikaCetera <ceteraerika@gmail.com>
2025-01-21 18:13:32 +0100

    feat:join queries

---
+++ join-queries.sql
@@ -0,0 +1,62 @@
+--1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+--1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.*, `degrees`.`name` AS `degree_name`
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di Laurea in Economia";
+
+
+--2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+SELECT `degrees`. * , `departments`.`name` AS `department_name`
+FROM `degrees`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`. `name` = "Dipartimento di Neuroscienze"
+AND `degrees`.`level` = "magistrale";
+
+--3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.*, `courses`.`name` AS `course_name`
+FROM `courses`
+INNER JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `teachers`
+ON `course_teacher`. `teacher_id` = `teachers`.`id`
+WHERE `teachers`.`id` = 44
+
+--4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS `degrees_name`,  `departments`.`name` AS `department_name`
+FROM `students`
+INNER JOIN  `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname`, `students`.`name` ASC
+
+--5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.*, `courses`.`name` AS `course_name`, `teachers`.`name` AS `teacher_name`
+FROM `degrees`
+INNER JOIN `courses`
+ON `courses`.`degree_id` = `degrees`.`id`
+INNER JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+ORDER BY `degrees`.`name`
+
+--6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT DISTINCT `teachers`.*, `departments`.`name` AS `department_name`
+FROM `teachers`
+INNER JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+INNER JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+INNER JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+INNER JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "Dipartimento di Matematica"
+ORDER BY `teachers`.`surname`, `teachers`.`name`;
+
+
+--7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
--- queries.sql
+++ queries.sql
@@ -1,38 +1,39 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT COUNT(*) 
+SELECT * 
 FROM `students` 
 WHERE YEAR(`date_of_birth`) = 1990
+ORDER BY `date_of_birth`;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-SELECT COUNT(*)
+SELECT *
 FROM `courses` 
 WHERE `cfu`> 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni 
- SELECT *
+SELECT *, timestampdiff(YEAR , `date_of_birth`, CURDATE()) AS `age`
 FROM `students`
-WHERE YEAR(`date_of_birth`) <= 1994
-ORDER BY  `date_of_birth`;
+WHERE timestampdiff(YEAR , `date_of_birth`, CURDATE()) > 30 --correggere
+ORDER BY  `age`;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-SELECT COUNT(*)
+SELECT *
 FROM `courses`
 WHERE `year` = 1
 AND `period` = "I semestre";
 
 --5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-SELECT COUNT(*)
+SELECT *
 FROM `exams`
 WHERE `date` = "2020-06-20"
 AND HOUR (`hour`) >= 14;
 
 --6. Selezionare tutti i corsi di laurea magistrale (38)
-SELECT COUNT(*)
+SELECT *
 FROM `degrees`
 WHERE `level` = "magistrale";
 
 --7. Daquanti dipartimenti è composta l'università? (12)
-SELECT COUNT(*)
+SELECT COUNT(*) AS `number_departments`
 FROM `departments`;
 
 --8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
