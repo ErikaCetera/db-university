e0dce3214bee9566219b7ff3f4f75fd9c234c9d4
ErikaCetera <ceteraerika@gmail.com>
2025-01-21 19:25:40 +0100

    feat: bonus join queries

--- join-queries.sql
+++ join-queries.sql
@@ -22,7 +22,7 @@ INNER JOIN `course_teacher`
 ON `courses`.`id` = `course_teacher`.`course_id`
 INNER JOIN `teachers`
 ON `course_teacher`. `teacher_id` = `teachers`.`id`
-WHERE `teachers`.`id` = 44
+WHERE `teachers`.`id` = 44;
 
 --4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS `degrees_name`,  `departments`.`name` AS `department_name`
@@ -31,7 +31,7 @@ INNER JOIN  `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 INNER JOIN `departments`
 ON `degrees`.`department_id` = `departments`.`id`
-ORDER BY `students`.`surname`, `students`.`name` ASC
+ORDER BY `students`.`surname`, `students`.`name` ASC;
 
 --5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 SELECT `degrees`.*, `courses`.`name` AS `course_name`, `teachers`.`name` AS `teacher_name`
@@ -42,7 +42,7 @@ INNER JOIN `course_teacher`
 ON `courses`.`id` = `course_teacher`.`course_id`
 INNER JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`
-ORDER BY `degrees`.`name`
+ORDER BY `degrees`.`name`;
 
 --6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 SELECT DISTINCT `teachers`.*, `departments`.`name` AS `department_name`
@@ -60,3 +60,15 @@ ORDER BY `teachers`.`surname`, `teachers`.`name`;
 
 
 --7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT COUNT(`exam_student`.`vote`) AS `sostenuti`, `students`.`surname` AS `student_surname`, `students`.`name` AS `student_name`,
+ `students`.`id` AS `student_id`
+FROM`students`
+INNER JOIN `exam_student` 
+ON `students`.`id` = `exam_student`.`student_id`
+INNER JOIN `exams` 
+ON `exams`.`id` = `exam_student`.`exam_id`
+INNER JOIN `courses` 
+ON `courses`.`id` = `exams`.`course_id`
+WHERE `exam_student`.`vote` < 18
+GROUP BY `students`.`id`
+ORDER BY `students`.`surname`, `students`.`name`;
